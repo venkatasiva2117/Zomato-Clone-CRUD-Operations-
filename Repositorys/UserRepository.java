141f45cd29cd046a2146f1101e5d955c1d254295
venkatasiva2117 <venkatasivakumar32@gmail.com>
2025-11-08 17:50:44 +0530

    Add files via upload

---
+++ Repositorys/UserRepository.java
@@ -0,0 +1,12 @@
+package com.food.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.food.entity.User;
+
+public interface UserRepository extends CrudRepository<User, Integer>{
+
+	public Optional<User> findById(int id);
+}
